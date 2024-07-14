56f0e420be0d33ee875d4a2e386bdee262d8fc61
Shariq <shariqsd2003@gmail.com>
2024-07-14 16:21:37 +0530

    Final Touch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Cards from "./components/Cards"
+import data from './data'
+
 
 function App() {
+  const [tours,setTours]=useState(data)
+  function removeHandler(id){
+    setTours(tours.filter(tour=>
+       tour.id !== id
+    ))
+  }
+
+  if(tours.length===0){
+    return (
+      <>
+        <div className='refresh-container'>
+          <h1 className=''>No Tour Left</h1>
+          <button onClick={()=>{
+            setTours(data);
+          }} className='refresh-btn'>Refresh</button>
+        </div>
+      </>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className='heading'>Plan With Shariq</h1>
+        <Cards tours={tours} removeHandler={removeHandler}/>
+      </div>
+    </>
   );
 }
 
